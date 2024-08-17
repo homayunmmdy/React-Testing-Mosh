bebac8f33a741336799e3f6d9320a82a5f312437
Homayoun <homayoun763@gmail.com>
2024-08-17 20:51:20 +0330

    Search Box example

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,9 +1,8 @@
-import Onboarding from "../components/Onboarding";
-import ExpandableText from "../components/ExpandableText";
+import SearchBox from "../components/SearchBox";
 
 const PlaygroundPage = () => {
   return (
-    <ExpandableText text="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aut culpa aperiam quae dolore, hic reiciendis? Assumenda dolore culpa explicabo doloremque, provident cupiditate velit optio molestiae maxime ut nesciunt soluta nam eligendi ducimus officiis iusto quas hic cum ad placeat omnis labore voluptatem incidunt dicta! Dolore corporis, possimus numquam minima, non sint inventore libero, et a voluptatibus cupiditate itaque animi eius? Dolore explicabo illo aperiam eius? Asperiores nobis sunt deserunt laborum numquam aut qui consectetur beatae, iusto natus ducimus harum enim dicta optio molestias reprehenderit consequatur quibusdam ex. Laudantium vero, velit quaerat at nobis deleniti voluptatum consequuntur vitae qui ratione voluptates!" />
+    <SearchBox onChange={(text) => console.log(text)}/>
   );
 };
 
