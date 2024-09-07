b857f914715af4211a7ede3df5344e95ac91d8c9
Homayoun <homayoun763@gmail.com>
2024-09-07 19:55:23 +0330

    Parameterizing Tests

--- tests/components/ProductForm.test.tsx
+++ tests/components/ProductForm.test.tsx
@@ -74,12 +74,24 @@ describe("ProductForm", () => {
     expect(nameInput).toHaveFocus();
   });
 
-  it('should display an error if name is missing',async () => {
+  it.each([
+    {
+      scenario: 'missing',
+      errorMessage: /required/i
+    },
+    {
+      scenario: 'longer than 255 characters',
+      name: 'a'.repeat(256),
+      errorMessage: /255/i
+    },
+  ])('should display an error if name is $scenario',async ({name,  errorMessage}) => {
     const { waitForFormToLoad } = renderComponent();
 
     const form = await waitForFormToLoad();
 
     const user = userEvent.setup()
+    if(name !== undefined)
+       await user.type(form.nameInput, name)
     await user.type(form.priceInput, '10');
     await user.click(form.categoryInput);
     const options = screen.getAllByRole('option');
@@ -88,6 +100,6 @@ describe("ProductForm", () => {
 
     const error = screen.getByRole('alert')
     expect(error).toBeInTheDocument()
-    expect(error).toHaveTextContent(/required/i);
+    expect(error).toHaveTextContent(errorMessage);
   })
 });
