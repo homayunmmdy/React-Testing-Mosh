6d5cb8cd6fd0e893e2b91a6f08e6d3b0bc0ca05e
Homayoun <homayoun763@gmail.com>
2024-09-10 00:19:58 +0330

    Testing Invalid Routes

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -28,4 +28,10 @@ describe("Router", () => {
 
     db.product.delete({ where: { id: { equals: product.id } } });
   });
+
+  it('should render the not found page for invalid routes', () => {
+    navigateTo('/invalid-route');
+
+    expect(screen.getByText(/not found/i)).toBeInTheDocument()
+  })
 });
