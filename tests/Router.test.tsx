889d521ce0485cbd2f6d11dbc30ea02b43cd572a
Homayoun <homayoun763@gmail.com>
2024-09-10 00:16:52 +0330

    Testing Routes with Parameters

--- tests/Router.test.tsx
+++ tests/Router.test.tsx
@@ -1,5 +1,6 @@
 import { screen } from "@testing-library/react";
 import { navigateTo } from "./utils";
+import { db } from "./mocks/db";
 
 describe("Router", () => {
   it("should render the home page for /", () => {
@@ -14,5 +15,17 @@ describe("Router", () => {
     expect(
       screen.getByRole("heading", { name: /products/i })
     ).toBeInTheDocument();
+  });
+
+  it("should render the products details page for /products/:id", async () => {
+    const product = db.product.create();
+
+    navigateTo("/products/" + product.id);
+
+    expect(
+      await screen.findByRole("heading", { name: product.name })
+    ).toBeInTheDocument();
+
+    db.product.delete({ where: { id: { equals: product.id } } });
   });
 });
